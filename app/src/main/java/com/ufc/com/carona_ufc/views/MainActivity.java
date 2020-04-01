cc94d53db54fb98a33a8af1facda34482ff7cb26
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-04-01 12:15:27 -0300

    Adicionado alguns detalhes no layout

--- app/src/main/java/com/ufc/com/carona_ufc/views/MainActivity.java
+++ app/src/main/java/com/ufc/com/carona_ufc/views/MainActivity.java
@@ -1,17 +1,21 @@
 package com.ufc.com.carona_ufc.views;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ufc.com.carona_ufc.OferecerCaronaActivity;
 import com.ufc.com.carona_ufc.R;
 
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnOferecerCarona;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +24,19 @@ public class MainActivity extends AppCompatActivity {
         ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#7E5DCA")));
 
+        btnOferecerCarona = findViewById(R.id.btnOferecerCarona);
 
+
+
+
+
+        btnOferecerCarona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), OferecerCaronaActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
